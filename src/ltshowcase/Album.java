af27bb33d1d4f05ede9938647ab89b07848798a3
jess-alexander <alexanderjessmarie@gmail.com>
2017-04-21 22:50:40 -0500

    update

---
+++ src/ltshowcase/Album.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ltshowcase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Jess
+ */
+public class Album {
+    int albumKey;
+    
+    int getAlbumKey(){
+        return albumKey;
+    }
+    void setAlbumKey(int albumKey){
+        this.albumKey = albumKey;
+    }
+}
